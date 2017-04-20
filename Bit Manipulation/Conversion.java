fbf57be2977e08fd77e5e13944e5ac371d46de93
nayakan <nayakj9@gmail.com>
2017-04-20 15:28:39 -0700

    Create Conversion.java

---
+++ Bit Manipulation/Conversion.java
@@ -0,0 +1,17 @@
+/*
+ * Write a function to determine the number of bits you would
+ * need to flip to convert interger A to integer B
+ * Example 
+ * Input: 29 (11101) to 15(01111)
+ * Output: 2
+ */
+
+public class Conversion {
+    public static int bitSwap(int a, int b) {
+        int count = 0;
+        for(int c = a ^ b; c != 0; c >>= 1) {
+            count += c & 1;
+        }
+        return count;
+    }
+}

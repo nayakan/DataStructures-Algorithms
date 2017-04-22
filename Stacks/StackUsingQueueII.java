7ddf545b08249dad6a497dc42666cdf16d1e7bdc
nayakan <nayakj9@gmail.com>
2017-04-22 13:34:31 -0700

    Create StackUsingQueueII.java

---
+++ Stacks/StackUsingQueueII.java
@@ -0,0 +1,35 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Implement stack using single queue.
+ */
+
+public class StackUsingQueueII {
+    //one Queue solution
+    private Queue<Integer> q = new LinkedList<Integer>();
+
+    // Push element x onto stack.
+    public void push(int x) {
+        q.add(x);
+        for(int i = 1; i < q.size(); i ++) { //rotate the queue to make the tail be the head
+            q.add(q.poll());
+        }
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        q.poll();
+    }
+
+    // Get the top element.
+    public int top() {
+        return q.peek();        
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}

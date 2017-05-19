7e02ed31fe978807ae4257d2ee31c60f6ba04d60
nayakan <nayakj9@gmail.com>
2017-05-19 14:13:51 -0700

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,1 +1,29 @@
 
+public class SelectionSort {
+    public static void sort(int[] arr) {
+       for(int i = 0; i < arr.length - 1; i++) {
+           int minIndex = i;
+           for(int j = i + 1; j < arr.length; j++) {
+               if(arr[j] < arr[minIndex]) {
+                   minIndex = j;
+               }
+           }
+           
+           int temp = arr[minIndex];
+           arr[minIndex] = arr[i];
+           arr[i] = temp;
+       }
+    }
+    
+    public static void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 6, 2, 4, 3};
+        sort(arr);
+        printArray(arr);
+    }
+}

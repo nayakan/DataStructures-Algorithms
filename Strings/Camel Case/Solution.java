d30dfa4c9adc134e981001a291818f4fa922d838
nayakan <nayakj9@gmail.com>
2017-06-05 17:29:17 -0700

    Create Solution.java

---
+++ Strings/Camel Case/Solution.java
@@ -0,0 +1,11 @@
+ 
+![screen shot 2017-06-05 at 17 28 48](https://cloud.githubusercontent.com/assets/17459420/26808820/7a18ec66-4a14-11e7-8776-61f78da32758.png)
+
+  
+  public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+    }
+  }
